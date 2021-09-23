bbf00aa0d9f72bf0b35e0c5e119b4cfe98390614
Leidy <leidyjoanne86@gmail.com>
2021-09-23 16:40:58 -0500

    first commit

--- src/modules/core/controllers/indexController.ts
+++ src/modules/core/controllers/indexController.ts
@@ -2,9 +2,7 @@ import { Request, Response } from 'express';
 
 class IndexController {
   public Health(rq: Request, rs: Response) {
-    console.log('entra al controller');
     rs.status(200);
-    rs.end('Hola mundo este controller quedo al pelo');
   }
 }
 
